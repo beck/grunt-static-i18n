66913b4f32eeffe28205c726b71f8792ec53e503
Doug Beck <doug@douglasbeck.com>
2014-03-02 12:13:00 -0500

    Tests: stub grunt.log.warn

--- test/statici18n_test.js
+++ test/statici18n_test.js
@@ -1,15 +1,21 @@
-/*global describe, before, it*/
+/*global after, before, describe, it*/
 'use strict';
 var assert  = require('assert');
 var grunt = require('grunt');
 var path = require('path');
+var sinon = require('sinon');
 
 var statici18n = require('../tasks/statici18n');
 statici18n(grunt);
 
 describe('exists', function() {
-  it('returns false with fake file', function() {
+  after(function() {
+    grunt.log.warn.restore();
+  });
+  it('should nag and filter if the file is missing', function() {
+    sinon.stub(grunt.log, 'warn');
     assert.equal(false, statici18n.exists('some/fake/file.txt'));
+    sinon.assert.calledOnce(grunt.log.warn);
   });
   it('returns true with some real file', function() {
     var readme = path.join(__dirname, '..', 'README.md');
