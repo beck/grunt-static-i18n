3b21f3b7a3dc8304554c97f1fcc1892bf07e2b09
Doug Beck <doug@douglasbeck.com>
2014-03-02 15:17:32 -0500

    Add grunt task to create fixture locale

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,11 +33,38 @@ module.exports = function(grunt) {
       all: { src: ['test/**/*_test.js'] }
     },
 
+    // abideExtract is limited by the parsers used in jsxgettext
+    // grunt-xgettext works on text because it regexs the source
+    xgettext: {
+      options: {
+        functionName: '_',
+        potFile: 'test/fixtures/app/locale/template/messages.pot',
+      },
+      fixtureJSON: {
+        files: {
+          javascript: ['test/fixtures/app/**/*.json'],
+        },
+      }
+    },
+
+    abideCreate: {
+      fixture: {
+        options: {
+          template: '<%= xgettext.options.potFile %>',
+          languages: ['en-gb', 'fr'],  // fancy, changes to locales on create
+          localeDir: 'test/fixtures/app/locale',
+        }
+      }
+    }
+
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
+  grunt.registerTask('makemessages', [
+    'xgettext', 'xgettext', 'abideCreate'
+  ]);
   grunt.registerTask('test', ['simplemocha']);
 
   // By default, lint and run all tests.
